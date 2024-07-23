87fef4a920dac815a6975c33660cd4d77e4fc95f
kaoruyuki <>
2024-07-23 11:59:32 +0900

    Edit: Login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import type { AppProps } from "next/app"
 import { useState, useEffect } from "react"
 import Cookies from "js-cookie"
 import Layout from "@/components/layout"
-import { AppContext, AppProvider } from "@/context/AppContext"
+import { AppProvider } from "@/context/AppContext"
+import { useRouter } from "next/router"
 
 export default function App({ Component, pageProps } : AppProps) {
   const [user, setUser] = useState(null)
-
   useEffect(() => {
     const token = Cookies.get("token")
     if (token) {
@@ -16,16 +16,24 @@ export default function App({ Component, pageProps } : AppProps) {
           Authorization : `Bearer ${token}`
         }
       }).then(async (res) => {
-        if (!res.ok) {
+        if (res.ok) {
+          const user = await res.json()
+          setUser(user)
+        } else {
           Cookies.remove("token")
           setUser(null)
-          return null
         }
-        const user = await res.json()
-        setUser(user)
       })
     }
   }, [])
+
+  const router = useRouter()
+  useEffect(() => {
+    const token = Cookies.get("token")
+    if (!token && router.pathname !== "/login" && router.pathname !== "/signup") {
+      router.push("/login")
+    }
+  }, [router])
 
   return (
     <AppProvider value={{ user : user, setUser : setUser }}>
